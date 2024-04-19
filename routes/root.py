4e7bdf0568eaf81adaa54ef4fd13ddcf804b69c7
i am doraemon <khunball46@gmail.com>
2024-04-19 15:50:57 +0700

    test api can use(fix by Qus)

--- routes/root.py
+++ routes/root.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 
 from database.connection import get_session
-from database.schemas import test
+from database.schemas import test, TestTable
 
 router = APIRouter()
 
@@ -15,6 +16,7 @@ async def root():
 
 
 @router.get("/test")
-def testQuery(db: Session = Depends(get_session)):
-    results = db.query(test).all()
-    return {"user": results}
+async def testQuery(db: AsyncSession = Depends(get_session)):
+    results = await db.execute(select(TestTable))
+    users = results.scalars().all()
+    return {"user": users}
