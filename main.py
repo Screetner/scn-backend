90391c58ed907fe8f1f385b1c594f55214f4655e
AuJung <nuttapong6280@gmail.com>
2024-06-09 15:54:23 +0700

    feat: allow cors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database.connection import Base, engine
 from routes import *
@@ -14,5 +15,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(root_router)
 app.include_router(auth_router)
